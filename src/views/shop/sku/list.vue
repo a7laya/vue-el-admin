ecd72ac1e094ed9170f580229553cdcaac7de008
a7laya <75739680@qq.com>
2020-04-06 22:51:28 +0800

    123

--- src/views/shop/sku/list.vue
+++ src/views/shop/sku/list.vue
@@ -2,7 +2,7 @@
 	<div class="bg-white h-100 px-3 py-2" style="margin:-8px -20px 20px -20px; ">
 
 		<div>
-			<el-button size="medium" type="success" @click="createModel = true">添加规格</el-button>
+			<el-button size="medium" type="success" @click="openModel">添加规格</el-button>
 			<el-button size="medium" type="danger">批量删除</el-button>
 		</div>
 		<!-- 表格 -->
@@ -22,7 +22,7 @@
 				<template slot-scope="scope">
 					<el-button-group>
 						<el-button plain type="success" size="mini">编辑</el-button>
-						<el-button plain type="warning" size="mini">删除</el-button>
+						<el-button plain type="warning" size="mini" @click="deleteItem(scope)">删除</el-button>
 					</el-button-group>
 				</template>
 			</el-table-column>
@@ -37,19 +37,36 @@
 			</div>
 		</el-footer>
 		<!-- 新增 | 修改规格模态框 -->
-		<el-dialog title="添加规格" :visible.sync="createModel">
+		<el-dialog title="添加规格" :visible.sync="createModel" top="5vh">
 			<!-- 表单内容 -->
-			<el-form ref="form" :model="form" label-width="80px">
-				<el-form-item label="规格名称">
-					<el-input v-model="form.name" placeholder="规格名称" size="mini"></el-input>
+			<el-form ref="form" :rules="rules" :model="form" label-width="80px">
+				<el-form-item label="规格名称" prop="name">
+					<el-input v-model="form.name" placeholder="规格名称" size="mini" class="w-50"></el-input>
 				</el-form-item>
 				<el-form-item label="排序">
 					<el-input-number v-model="form.order" :min="0" size="mini"></el-input-number>
 				</el-form-item>
+				<el-form-item label="状态">
+					<el-radio-group v-model="form.status" size="mini">
+						<el-radio :label="1" border>启用</el-radio>
+						<el-radio :label="0" border>禁用</el-radio>
+					</el-radio-group>
+				</el-form-item>
+				<el-form-item label="类型">
+					<el-radio-group v-model="form.type" size="mini">
+						<el-radio :label="0" border>文字</el-radio>
+						<el-radio :label="1" border>颜色</el-radio>
+						<el-radio :label="2" border>图片</el-radio>
+					</el-radio-group>
+				</el-form-item>
+				<el-form-item label="规格值" prop="value">
+					<el-input type="textarea" :rows="2" v-model="form.value" 
+					placeholder="一行一个规格项,多个规格项用换行来输入" size="mini"></el-input>
+				</el-form-item>
 			</el-form>
 			<div slot="footer" class="dialog-footer">
 				<el-button @click="createModel = false">取 消</el-button>
-				<el-button type="primary" @click="createModel = false">确 定</el-button>
+				<el-button type="primary" @click="submit">确 定</el-button>
 			</div>
 		</el-dialog>
 	</div>
@@ -70,6 +87,15 @@
 					type: 0,
 					value: "",
 				},
+				// 表单验证
+				rules:{
+					name: [
+						{required: true, message: "规格名称不能为空", trigger: "blur"},
+					],
+					value: [
+						{required: true, message: "规格项目不能为空", trigger: "blur"},
+					]
+				},
 				tableData: [
 					{
 						name: '颜色', 
@@ -87,6 +113,19 @@
 		created() {
 		},
 		methods: {
+			// 打开模态框
+			openModel(){
+				// 初始化表单
+				this.form = {
+					name: '',
+					order: 50,
+					status: 1,
+					type: 0,
+					value: "",
+				}
+				// 打开dialog
+				this.createModel = true
+			},
 			// 改变规格状态
 			changeStatus(item){
 				item.status = item.status === 1 ?  0 : 1
@@ -94,6 +133,34 @@
 					type: 'success',
 					message: item.status === 1 ? `已启用: ${item.name}` : `已禁用: ${item.name}`,
 				});
+			},
+			// 提交规格
+			submit(){
+				// 验证成功后才提交
+				this.$refs.form.validate(res => {
+					if(!res) return
+					this.form.value = this.form.value.replace('\n',',')
+					this.tableData.unshift({...this.form})
+					this.createModel = false
+					this.$message({
+						message: '添加成功',
+						type: 'success'
+					});
+				})
+			},
+			// 删除单个规格
+			deleteItem(scope){
+				this.$confirm('确定删除: ' + scope.row.name, '提示', {
+					confirmButtonText: '确定',
+					cancelButtonText: '取消',
+					type: 'warning'
+				}).then(() => {
+					this.tableData.splice(scope.$index,1)
+					this.$message({
+						message: '删除成功',
+						type: 'success'
+					});
+				})
 			},
 			// 表格多选
 			handleSelectionChange(val) {
@@ -111,4 +178,8 @@
 	};
 </script>
 
-<style></style>
+<style>
+	.el-radio {
+		margin-right: 0;
+	}
+</style>
